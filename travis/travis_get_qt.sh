8712b7bd27f23e7f4ae5a18383e4be6b6640704d
osmsc <nwilson@osmsc.com>
2017-01-04 22:14:52 -0800

    fixing bug in qt download script

--- travis/travis_get_qt.sh
+++ travis/travis_get_qt.sh
@@ -5,10 +5,10 @@ export PARENT_URL=http://simvascular.stanford.edu/downloads/public/open_source/l
 sudo mkdir -p /opt/Qt5.4.2
 sudo chmod -R a+rwx /opt/Qt5.4.2
 
-sudo mkdir -p /tmp/tarfiles
-pushd /tmp/tarfiles
+mkdir -p ~/tmp/tarfiles
+pushd ~/tmp/tarfiles
 wget $PARENT_URL/Qt5.4.2-ubuntu-14.04.tar.gz
 echo "untarring (Qt5.4.2-ubuntu-14.04.tar.gz)..."
-sudo tar --directory=/ -xzf $i
+sudo tar --directory=/ -xzvf ./Qt5.4.2-ubuntu-14.04.tar.gz
 rm Qt5.4.2-ubuntu-14.04.tar.gz
 popd
