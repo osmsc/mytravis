aead7095cafa8d4ab62455b6bbe4f3f8360245c4
osmsc <nwilson@osmsc.com>
2017-01-04 23:44:20 -0800

    tell which file is being compiled

--- BuildWithMake/MakeHelpers/rules.x64_linux.mk
+++ BuildWithMake/MakeHelpers/rules.x64_linux.mk
@@ -20,31 +20,39 @@ $(BUILD_MPI_DIR)/%.d: %.c
               [ -s $@ ] || rm -f $@'
 
 $(BUILD_DIR)/%.o: %.cxx
+	$(SV_QUIET_FLAG)echo "  building src ($<)"
 	$(SV_QUIET_FLAG)$(CXX) $(CXXFLAGS) -c $< -o $@
 
 $(BUILD_DIR)/%.o: %.c
+	$(SV_QUIET_FLAG)echo "  building src ($<)"
 	$(SV_QUIET_FLAG)$(CC) $(CCFLAGS) -c $< -o $@
 
 $(BUILD_DIR)/%.o: %.f
+	$(SV_QUIET_FLAG)echo "  building src ($<)"
 	$(SV_QUIET_FLAG)$(F90) $(FFLAGS) -c $< -o $@
 
 $(BUILD_DIR)/%.o: %.f90
+	$(SV_QUIET_FLAG)echo "  building src ($<)"
 	$(SV_QUIET_FLAG)$(F90) $(FFLAGS) -c $< -o $@
 
 $(BUILD_MPI_DIR)/%.o: %.cxx
 	$(SV_QUIET_FLAG)mkdir -p $(dir $@)
+	echo "  building src ($<)"
 	$(SV_QUIET_FLAG)$(CXX) $(CXXFLAGS) -c $< -o $@
 
 $(BUILD_MPI_DIR)/%.o: %.c
 	$(SV_QUIET_FLAG)mkdir -p $(dir $@)
+	$(SV_QUIET_FLAG)echo "  building src ($<)"
 	$(SV_QUIET_FLAG)$(CC) $(CCFLAGS) -c $< -o $@
 
 $(BUILD_MPI_DIR)/%.o: %.f
 	$(SV_QUIET_FLAG)mkdir -p $(dir $@)
+	$(SV_QUIET_FLAG)echo "  building src ($<)"
 	$(SV_QUIET_FLAG)$(F90) $(FFLAGS) -c $< -o $@
 
 $(BUILD_MPI_DIR)/%.o: %.f90
 	$(SV_QUIET_FLAG)mkdir -p $(dir $@)
+	$(SV_QUIET_FLAG)echo "  building src ($<)"
 	$(SV_QUIET_FLAG)$(F90) $(FFLAGS) -c $< -o $@
 
 endif
